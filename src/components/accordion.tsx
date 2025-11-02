13bee3cec745ee537a8b9f08ba0295b9aafd7237
pratitb <pratit.bangdiwala7@gmail.com>
2025-11-02 17:01:56 +0530

    fix: font sizes

--- src/components/accordion.tsx
+++ src/components/accordion.tsx
@@ -8,12 +8,12 @@ const Accordion = ({ title, content, getActionFn, isOpen }: AccordionType) => {
         <div className="w-full py-4 border-b border-gray-100 last:border-b-0" onClick={getActionFn}>
             {/* HEADER */}
             <div className="flex justify-between items-center cursor-pointer">
-                <p className="text-title font-semibold leading-5">{title}</p>
+                <p className="text-title font-semibold leading-5 lg:text-lg">{title}</p>
                 <img src={isOpen ? Assets?.Minus : Assets?.Plus} alt="" />
             </div>
             {/* CONTENT */}
             <div className={clsx('overflow-hidden', isOpen ? 'max-h-40 opacity-1' : 'max-h-0 opacity-0')}>
-                <p className="text-context text-sm pt-3 md:py-0">{content}</p>
+                <p className="text-context text-sm pt-3 md:py-0 lg:text-base">{content}</p>
             </div>
         </div>
     )
--- src/components/faqHeader.tsx
+++ src/components/faqHeader.tsx
@@ -7,7 +7,7 @@ const FaqHeader = ({ title }: FaqHeaderType) => {
             {/* FAQ HEADER */}
             <div className='flex items-center gap-4'>
                 <img src={Assets?.Star} alt="" />
-                <p className='text-title font-bold text-3xl md:text-4xl'>{title}</p>
+                <p className='text-title font-bold text-3xl md:text-4xl lg:text-5xl'>{title}</p>
             </div>
         </>
     )
